07de08a96ec7356b618d3dc288505188f59d229c
Juan Jose Cardona Gil <cardonagiljuanjose@gmail.com>
2024-09-12 18:38:26 -0500

    perfil visualisacion unica

--- perfil/views.py
+++ perfil/views.py
@@ -7,3 +7,8 @@ class PerfilView(LoginRequiredMixin, ListView):
     model = CustomUser
     template_name = 'perfil/perfil.html'
     context_object_name = 'users'
+
+    def get_queryset(self):
+        print(f'{CustomUser.objects.filter(username=self.request.user.username)}')
+        print('hola')
+        return CustomUser.objects.filter(username=self.request.user.username)
