ce3cc14221874c5dc0660a8bcc16c97f04992002
Sthefaniag <sthefaniaguevara.m@academia.umb.edu.co>
2024-09-29 14:49:13 -0500

    conexion de apis para prosodia cancion solo un butons

---
+++ prosodia/static/js/grabar_audio.js
@@ -0,0 +1,67 @@
+let mediaRecorder; // Variable para almacenar el objeto MediaRecorder
+let audioChunks = []; // Array para almacenar los fragmentos de audio grabados
+let isRecording = false; // Variable para rastrear el estado de la grabación
+
+document.getElementById('recordButton').addEventListener('click', () => {
+    if (!isRecording) {
+        // Iniciar grabación
+        navigator.mediaDevices.getUserMedia({ audio: true }) // Solicitar acceso al micrófono
+            .then(stream => {
+                mediaRecorder = new MediaRecorder(stream); // Crear un nuevo MediaRecorder con el stream de audio
+                mediaRecorder.start(); // Iniciar la grabación
+                isRecording = true; // Actualizar el estado de grabación
+                document.getElementById('status').textContent = 'Grabando...'; // Actualizar el texto de estado
+                document.getElementById('recordButton').textContent = 'Stop Recording'; // Cambiar el texto del botón
+
+                mediaRecorder.addEventListener('dataavailable', event => {
+                    audioChunks.push(event.data); // Añadir los fragmentos de audio grabados al array
+                });
+
+                mediaRecorder.addEventListener('stop', () => {
+                    const audioBlob = new Blob(audioChunks, { type: 'audio/wav' }); // Crear un Blob con los fragmentos de audio
+                    const formData = new FormData(); // Crear un nuevo FormData para enviar datos de formulario
+                    formData.append('audio', audioBlob, 'grabacion.wav'); // Añadir el Blob de audio al FormData
+
+                    // Enviar el FormData al servidor usando fetch
+                    fetch('http://127.0.0.1:8000/prosodia/prosodia_cancion/', {
+                        method: 'POST', // Especificar el método HTTP como POST
+                        headers: {
+                            'X-CSRFToken': getCookie('csrftoken') // Incluir el token CSRF en los encabezados
+                        },
+                        body: formData // Establecer el cuerpo de la solicitud como el FormData que contiene el archivo de audio
+                    })
+                    .then(response => response.json()) // Convertir la respuesta del servidor a JSON
+                    .then(data => {
+                        document.getElementById('resultado').textContent = `Resultado: ${data.result}`; // Mostrar el resultado del reconocimiento de voz en el elemento con id 'resultado'
+                    })
+                    .catch(error => console.error('Error:', error)); // Mostrar cualquier error que ocurra durante la solicitud en la consola
+
+                    document.getElementById('status').textContent = 'Grabación completa.'; // Actualizar el texto de estado
+                    document.getElementById('recordButton').textContent = 'Start Recording'; // Cambiar el texto del botón
+                    audioChunks = []; // Reiniciar el array de audioChunks para la próxima grabación
+                });
+            })
+            .catch(error => {
+                console.error('Error al acceder al micrófono:', error); // Mostrar cualquier error al acceder al micrófono en la consola
+            });
+    } else {
+        // Detener grabación
+        mediaRecorder.stop(); // Detener la grabación
+        isRecording = false; // Actualizar el estado de grabación
+    }
+});
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
--- prosodia/views.py
+++ prosodia/views.py
@@ -1,30 +1,97 @@
-from django.views.generic import ListView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Prosodia_cancion_info
-from .models import Prosodia_trabalenguas_info
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+import speech_recognition as sr
+from pydub import AudioSegment
+from io import BytesIO
 import random
+from django.shortcuts import render
 
-# Create your views here.
-class Prosodia_cancion_view(LoginRequiredMixin, ListView):
-    model = Prosodia_cancion_info
-    template_name = 'prosodia/prosodia_cancion.html'
-    context_object_name = 'instrucciones'
+from .models import Prosodia_cancion_info, Prosodia_trabalenguas_info
+from .serializer import ProsodiaCancionInfoSerializer, ProsodiaTrabalenguasInfoSerializer
 
-    def get_queryset(self):
-        longitud = len(Prosodia_cancion_info.objects.all())
+## Create your views here.
+#class Prosodia_cancion_view(LoginRequiredMixin, ListView):
+#    model = Prosodia_cancion_info
+#    template_name = 'prosodia/prosodia_cancion.html'
+#    context_object_name = 'instrucciones'
+#
+#    def get_queryset(self):
+#        longitud = len(Prosodia_cancion_info.objects.all())
+#        modulo_seleccionado = random.randint(1, longitud)
+#        queryset = Prosodia_cancion_info.objects.all()[modulo_seleccionado-1:modulo_seleccionado]  
+#        
+#        return queryset
+    
+class ProsodiaCancionAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, format=None):
+        longitud = Prosodia_cancion_info.objects.count()
         modulo_seleccionado = random.randint(1, longitud)
-        queryset = Prosodia_cancion_info.objects.all()[modulo_seleccionado-1:modulo_seleccionado]  
+        queryset = Prosodia_cancion_info.objects.all()[modulo_seleccionado-1:modulo_seleccionado]
+        # Contexto para pasar al template
+        context = {
+            'instrucciones': queryset
+        }
         
-        return queryset
+        # Renderiza el template HTML con el contexto
+        return render(request, 'prosodia/prosodia_cancion.html', context)
+    
+    def post(self, request, *args, **kwargs):
+        if 'audio' not in request.FILES:
+            return Response({'result': 'No se recibió ningún archivo de audio.'}, status=status.HTTP_400_BAD_REQUEST)
 
-class Prosodia_trabalenguas_view(LoginRequiredMixin, ListView):
-    model = Prosodia_trabalenguas_info
-    template_name = 'prosodia/prosodia_trabalenguas.html'
-    context_object_name = 'instrucciones'
+        # Leer el archivo de audio desde la solicitud
+        audio_file = request.FILES['audio']
+        
+        # Convertir el archivo de audio a formato compatible con SpeechRecognition
+        audio = AudioSegment.from_file(BytesIO(audio_file.read()))
+        audio_path = 'grabacion.wav'
+        audio.export(audio_path, format='wav')
 
-    def get_queryset(self):
-        longitud = len(Prosodia_trabalenguas_info.objects.all())
+        recognizer = sr.Recognizer()
+        with sr.AudioFile(audio_path) as source:
+            audio_data = recognizer.record(source)
+            try:
+                # Realizar reconocimiento de voz
+                texto = recognizer.recognize_google(audio_data, language='es-ES') 
+                print(f"Texto reconocido: {texto}")
+
+                # Palabra objetivo para verificar
+                palabra_objetivo = "hola"
+                if texto.lower() == palabra_objetivo.lower():
+                    result = "Exitoso"
+                else:
+                    result = f"Incorrecto. Usted dijo: '{texto}', pero la palabra correcta es: '{palabra_objetivo}'"
+            
+            except sr.UnknownValueError:
+                result = "No se pudo entender el audio."
+            except sr.RequestError as e:
+                result = f"Error al conectar con el servicio de reconocimiento de voz: {e}"
+
+        return Response({'result': result})
+
+#class Prosodia_trabalenguas_view(LoginRequiredMixin, ListView):
+#    model = Prosodia_trabalenguas_info
+#    template_name = 'prosodia/prosodia_trabalenguas.html'
+#    context_object_name = 'instrucciones'
+#
+#    def get_queryset(self):
+#        longitud = len(Prosodia_trabalenguas_info.objects.all())
+#        modulo_seleccionado = random.randint(1, longitud)
+#        queryset = Prosodia_trabalenguas_info.objects.all()[modulo_seleccionado-1:modulo_seleccionado]  
+#        
+#        return queryset 
+
+class ProsodiaTrabalenguasAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        longitud = Prosodia_trabalenguas_info.objects.count()
         modulo_seleccionado = random.randint(1, longitud)
-        queryset = Prosodia_trabalenguas_info.objects.all()[modulo_seleccionado-1:modulo_seleccionado]  
-        
-        return queryset 
+        queryset = Prosodia_trabalenguas_info.objects.all()[modulo_seleccionado-1:modulo_seleccionado]
+        serializer = ProsodiaTrabalenguasInfoSerializer(queryset, many=True)
+        return Response(serializer.data)
