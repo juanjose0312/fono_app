c080c12a07e41fc28922c3f0e82ec29228d340f2
Sthefaniag <sthefaniaguevara.m@academia.umb.edu.co>
2024-10-02 19:15:39 -0500

    audio ajustable segun el boton que se presione

--- prosodia/static/js/grabar_audio.js
+++ prosodia/static/js/grabar_audio.js
@@ -2,7 +2,7 @@ let mediaRecorder; // Variable para almacenar el objeto MediaRecorder
 let audioChunks = []; // Array para almacenar los fragmentos de audio grabados
 let isRecording = false; // Variable para rastrear el estado de la grabación
 
-document.getElementById('recordButton').addEventListener('click', () => {
+function FuncionBoton(idbutton, idstatus, idresultado) {
     if (!isRecording) {
         // Iniciar grabación
         navigator.mediaDevices.getUserMedia({ audio: true }) // Solicitar acceso al micrófono
@@ -10,8 +10,8 @@ document.getElementById('recordButton').addEventListener('click', () => {
                 mediaRecorder = new MediaRecorder(stream); // Crear un nuevo MediaRecorder con el stream de audio
                 mediaRecorder.start(); // Iniciar la grabación
                 isRecording = true; // Actualizar el estado de grabación
-                document.getElementById('status').textContent = 'Grabando...'; // Actualizar el texto de estado
-                document.getElementById('recordButton').textContent = 'Stop Recording'; // Cambiar el texto del botón
+                document.getElementById(idstatus).textContent = 'Grabando...'; // Actualizar el texto de estado
+                document.getElementById(idbutton).textContent = 'Stop Recording'; // Cambiar el texto del botón
 
                 mediaRecorder.addEventListener('dataavailable', event => {
                     audioChunks.push(event.data); // Añadir los fragmentos de audio grabados al array
@@ -32,24 +32,25 @@ document.getElementById('recordButton').addEventListener('click', () => {
                     })
                     .then(response => response.json()) // Convertir la respuesta del servidor a JSON
                     .then(data => {
-                        document.getElementById('resultado').textContent = `Resultado: ${data.result}`; // Mostrar el resultado del reconocimiento de voz en el elemento con id 'resultado'
+                        document.getElementById(idresultado).textContent = `Resultado: ${data.result}`; // Mostrar el resultado del reconocimiento de voz en el elemento con id 'resultado'
                     })
                     .catch(error => console.error('Error:', error)); // Mostrar cualquier error que ocurra durante la solicitud en la consola
 
-                    document.getElementById('status').textContent = 'Grabación completa.'; // Actualizar el texto de estado
-                    document.getElementById('recordButton').textContent = 'Start Recording'; // Cambiar el texto del botón
+                    document.getElementById(idstatus).textContent = 'Grabación completa.'; // Actualizar el texto de estado
+                    document.getElementById(idbutton).textContent = 'Start Recording'; // Cambiar el texto del botón
                     audioChunks = []; // Reiniciar el array de audioChunks para la próxima grabación
                 });
             })
-            .catch(error => {
-                console.error('Error al acceder al micrófono:', error); // Mostrar cualquier error al acceder al micrófono en la consola
-            });
-    } else {
-        // Detener grabación
+        .catch(error => {
+            console.error('Error al acceder al micrófono:', error); // Mostrar cualquier error al acceder al micrófono en la consola
+        });
+    } 
+    else{
         mediaRecorder.stop(); // Detener la grabación
         isRecording = false; // Actualizar el estado de grabación
+        document.getElementById('status').textContent = 'Grabación detenida'; // Actualizar el texto de estado
     }
-});
+};    
 
 function getCookie(name) {
     let cookieValue = null;
@@ -65,3 +66,9 @@ function getCookie(name) {
     }
     return cookieValue;
 }
+
+document.getElementById('recordButton_1').addEventListener('click', () => FuncionBoton("recordButton_1", "status_1", "resultado_1"));
+document.getElementById('recordButton_2').addEventListener('click', () => FuncionBoton("recordButton_2", "status_2", "resultado_2"));
+document.getElementById('recordButton_3').addEventListener('click', () => FuncionBoton("recordButton_3", "status_3", "resultado_3"));
+document.getElementById('recordButton_4').addEventListener('click', () => FuncionBoton("recordButton_4", "status_4", "resultado_4"));
+document.getElementById('recordButton_5').addEventListener('click', () => FuncionBoton("recordButton_5", "status_5", "resultado_5"));
