81671e79f4a97d4a4a38e6f335735253d9875657
Juan Jose Cardona Gil <cardonagiljuanjose@gmail.com>
2024-10-03 20:02:46 -0500

    refactorizacion view prosodia cancion : comprension de codigo

--- prosodia/views.py
+++ prosodia/views.py
@@ -17,21 +17,49 @@ class ProsodiaCancionAPIView(APIView):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.modulo_seleccionado = random.randint(
-            1,
-            Prosodia_cancion_info.objects.count()
-        )
+        cantidad_modulos = Prosodia_cancion_info.objects.count()
+        modulo_numero = random.randint(1,cantidad_modulos)
+        self.queryset = Prosodia_cancion_info.objects.all()[modulo_numero-1:modulo_numero]
         
     
     def get(self, request, format=None):
-        queryset = Prosodia_cancion_info.objects.all()[self.modulo_seleccionado-1:self.modulo_seleccionado]
+        
         # Contexto para pasar al template
         context = {
-            'instrucciones': queryset
+            'instrucciones': self.queryset
         }
         
         # Renderiza el template HTML con el contexto
         return render(request, 'prosodia/prosodia_cancion.html', context)
+    
+    def audio_a_texto(self, audio_file):
+        """"
+        Función para convertir un archivo de audio a texto.
+
+        Parámetros:
+            audio_file: Archivo de audio a convertir.
+        """
+        audio = AudioSegment.from_file(BytesIO(audio_file.read()))
+        audio_path = 'grabacion.wav'
+        audio.export(audio_path, format='wav')
+
+        recognizer = sr.Recognizer()
+        with sr.AudioFile(audio_path) as source:
+            audio_data = recognizer.record(source)
+            try:
+                result = recognizer.recognize_google(audio_data, language='es-CO').lower()
+                estado = True
+
+            except sr.UnknownValueError:
+                result = "No se pudo entender el audio."
+                estado = False
+
+            except sr.RequestError as e:
+                result = "Errorr al conectar con el servicio de reconocimiento de voz."
+                estado = False
+        
+        return result, estado
+        
     
     def post(self, request, *args, **kwargs):
         if 'audio' not in request.FILES:
@@ -42,34 +70,22 @@ class ProsodiaCancionAPIView(APIView):
         # Leer el archivo de audio desde la solicitud
         audio_file = request.FILES['audio']
         idbutton = request.POST['idbutton']
+
+        # Palabra objetivo para verificar
+        num_parrafo = idbutton.split('_')[1]
+        palabra_correcta = getattr(self.queryset[0], f'respuesta_{num_parrafo}')
+        palabra_audio, estado = self.audio_a_texto(audio_file)
         
-        # Convertir el archivo de audio a formato compatible con SpeechRecognition
-        audio = AudioSegment.from_file(BytesIO(audio_file.read()))
-        audio_path = 'grabacion.wav'
-        audio.export(audio_path, format='wav')
-
-        recognizer = sr.Recognizer()
-        with sr.AudioFile(audio_path) as source:
-            audio_data = recognizer.record(source)
-            try:
-                # Realizar reconocimiento de voz
-                texto = recognizer.recognize_google(audio_data, language='es-ES') 
-
-                # Palabra objetivo para verificar
-                num_parrafo = idbutton.split('_')[1]
-                query_set = list(Prosodia_cancion_info.objects.all()[self.modulo_seleccionado-1:self.modulo_seleccionado])
-                query = getattr(query_set[0], f'respuesta_{num_parrafo}')
-
-                palabra_objetivo = query
-                if texto.lower() == palabra_objetivo.lower():
-                    result = "Exitoso"
-                else:
-                    result = f"Incorrecto. Usted dijo: '{texto}', pero la palabra correcta es: '{palabra_objetivo}'"
-            
-            except sr.UnknownValueError:
-                result = "No se pudo entender el audio."
-            except sr.RequestError as e:
-                result = f"Error al conectar con el servicio de reconocimiento de voz: {e}"
+        if estado:
+            if palabra_audio == palabra_correcta.lower():
+                # Verificar si el texto es igual a la palabra objetivo
+                result = "¡Correcto!"
+            else:
+                # Si el texto no es igual a la palabra objetivo
+                result = f"¡Incorrecto! dijiste {palabra_audio}"
+        else:
+            # Si no se pudo entender el audio o fallo el servidor de reconocimiento de voz
+            result = palabra_audio
 
         return Response({'result': result})
 
